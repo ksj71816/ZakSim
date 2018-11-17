1751d6ac22daba4e7c85177df432a6452b474191
KimHyeoksoo <alive.khs@gmail.com>
2018-11-17 22:01:10 +0900

    페이징

--- ZakSim/src/main/java/zaksim/dao/CommunitySearchDAO.java
+++ ZakSim/src/main/java/zaksim/dao/CommunitySearchDAO.java
@@ -1,8 +1,10 @@
 package zaksim.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import zaksim.dto.CommunityGroup;
+import zaksim.dto.GroupLike;
 
 public interface CommunitySearchDAO {
 
@@ -14,5 +16,28 @@ public interface CommunitySearchDAO {
 	
 	// 키워드 검색
 	public List<CommunityGroup> searchKeywordGroup(String Keyword);
+
+	/**
+	 * 타이틀 검색 시 총 갯수
+	 * @param searchContent 
+	 * @return
+	 */
+	public int titleTotalCount(String searchContent);
+	
+	/**
+	 * 타이틀 검색 시 총 갯수
+	 * @param searchContent 
+	 * @return
+	 */
+	public int categoryTotalCount(String searchContent);
+
+	/**
+	 * 그룹 검색 (페이징 )
+	 * @param searchContent
+	 * @return
+	 */
+	public List<CommunityGroup> titlePage(Map<String, Object> map);
+
+	
 	
 }
